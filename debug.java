3e83898f5dd3a5194db7c7616ee2aaa83804077d
sashabeck <64157404+sashabeck@users.noreply.github.com>
2021-02-07 19:07:07 -0500

    Debug push

---
+++ debug.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class debug {
+
+	public static void main(String args[]) {
+		
+        float p, r, t, sinterest;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Principal : ");
+        p = scan.nextFloat();
+        System.out.print("Enter the Rate of interest : ");
+        r = scan.nextFloat();
+        System.out.print("Enter the Time period : ");
+        t = scan.nextFloat();
+        scan.close();
+        sinterest = p * r * t / 100;
+        System.out.print("Simple Interest is: " + sinterest);
+}
+}
